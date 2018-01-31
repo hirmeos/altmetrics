61313b34a842ac76dd86e908510b76f2bc812f55
James Hurford <james.hurford@ubiquitypress.com>
2018-01-31 14:23:36 +0000

    CSV processing task expects url, then doi
    Only one doi will be created but each doi can have multiple urls, which is created by the task.
    
    The task will only add a doi or url if the csv row column is not blank or empty.
    
    Will not add a new doi or url if they are already present, though nothing is stopping a duplicate url if the doi is different
    
    Redirects to list of uploaded files after uploading a csv for processing
    
    #3

--- src/importer/views.py
+++ src/importer/views.py
@@ -3,8 +3,9 @@ from urllib.parse import urljoin
 from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import FormView, ListView, TemplateView
+from django.views.generic import FormView, ListView, RedirectView
 
 from .forms import CSVUploadForm
 from .models import CSVUpload
@@ -15,21 +16,27 @@ class UploadCSVView(FormView):
     form_class = CSVUploadForm
     template_name = 'upload_form.html'
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-class SaveUploadView(TemplateView):
 
-    template_name = 'save.html'
+class SaveUploadView(RedirectView):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    permanent = False
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.url = reverse('importer-list')
 
         CSVUpload.objects.create(
-            file_name=context.get('file_name'),
-            link=urljoin(settings.S3_URL_TEMPLATE, context.get('file_name')),
+            file_name=kwargs.get('file_name'),
+            link=urljoin(settings.S3_URL_TEMPLATE, kwargs.get('file_name')),
             user=auth.get_user(self.request),
         )
 
-        return context
+        return super().dispatch(*args, **kwargs)
+
 
 
 class UploadsView(ListView):
