e2e5817fbf4ce3967e6588e895b2130987c8233a
James Hurford <james.hurford@ubiquitypress.com>
2018-02-05 14:55:30 +0000

    Made plugins require a PROVIDER var with a method of process(doi)
    Task for pulling metrics will go through all Dois that have not been checked or have not been chacked for 7 days
    
    PROVIDER.process must return a list of events with each being a json object with 'external_id', 'source_id', 'source', 'created_at' and 'content' keys
    
    #4

--- src/providers/crossref/crossref.py
+++ src/providers/crossref/crossref.py
@@ -9,5 +9,7 @@ class CrossrefEventDataProvider(GenericDataProvider):
     supported_sources = ['twitter', 'wikipedia', 'hypothes.is']
 
     def process(self, doi):
+        """doi is the DOI model"""
         client = CrossRefClient()
-        return [event for event in client.get_events(doi) if event.source in supported_sources]
+        return [event for event in client.get_events(doi.doi) if event.get('source') in self.supported_sources]
+
