dc368f27dac5d83658990ebfbe449b6438e023b4
rowan08 <g07h3212@gmail.com>
2019-02-22 18:30:30 +0000

    handle additional wikimedia links

--- src/processor/logic.py
+++ src/processor/logic.py
@@ -1,7 +1,9 @@
+from logging import getLogger
 import os
 import re
+import requests
 from itertools import chain
-from urllib.parse import unquote
+from urllib.parse import unquote, urlsplit
 
 import wikipedia
 from wikipedia import PageError
@@ -9,6 +11,7 @@ from wikipedia import PageError
 from core import db
 
 
+logger = getLogger(__name__)
 WIKI_PATTERN = re.compile('[a-z]{2}[.]wikipedia[.]org/.*')
 
 
@@ -16,13 +19,13 @@ def check_existing_entries(model_column, column_values):
     """ Get a tuple of values from a model, where entries with those values
     already exist in the database.
 
-        Args:
-            model_column (object): column to check in the form `Model.Column`
-            column_values (list): list of values to check - these
-                should ideally be unique for the model.
+    Args:
+        model_column (object): column to check in the form `Model.Column`.
+        column_values (list): list of values to check - these
+            should ideally be unique for the model.
 
-        Returns:
-            tuple: Values of entries that are already in the database
+    Returns:
+        tuple: Values of entries that are already in the database.
     """
     return tuple(
         chain.from_iterable(
@@ -35,21 +38,107 @@ def check_existing_entries(model_column, column_values):
     )
 
 
+def get_wiki_page_title(url):
+    """ Get the title of a Wikimedia page, based on its URL.
+
+    Args:
+        url (str): url of Wikimedia page.
+
+    Returns:
+        str: title of the Wikimedia page
+    """
+    url_obj = urlsplit(url)
+    return unquote(
+        os.path.basename(url_obj.path)
+    )
+
+
+def get_wiki_call_info(url):
+    """ Determine the URL and parameters to use query the Wikimedia API about
+    info on a specific page.
+
+    Args:
+        url (str): url of Wikimedia page.
+
+    Returns:
+        tuple: url and dict of parameters to use for the API call.
+    """
+    url_obj = urlsplit(url)
+    title = get_wiki_page_title(url)
+
+    call_params = {
+        'action': 'query',
+        'prop': 'extlinks',
+        'redirects': 'true',
+        'format': 'json',
+        'titles': title,
+    }
+    call_url = f'https://{url_obj.netloc}/w/api.php'
+
+    return call_url, call_params
+
+
+def get_wikipedia_references(url):
+    """ Get a list of references from a Wikipedia page.
+
+    Args:
+        url (str): url of Wikipedia page.
+
+    Returns:
+        list: references from the wikimedia page.
+    """
+    page_language = get_language_from_wiki_url(url)
+    page_title = get_wiki_page_title(url)
+
+    wikipedia.set_lang(page_language)
+
+    try:
+        page_obj = wikipedia.WikipediaPage(title=page_title)
+    except PageError:
+        page_obj = wikipedia.page(title=page_title)
+
+    return page_obj.references
+
+
+def get_non_wikipedia_references(url):
+    """ Get list of references from a Wikimedia page that is not Wikipedia.
+    e.g. wikisource.org, meta.wikimedia.org.
+
+    Args:
+        url (str): url of Wikimedia page.
+
+    Returns:
+        list: references/external links from the wikimedia page.
+    """
+    call_url, call_params = get_wiki_call_info(url)
+    response = requests.get(call_url, params=call_params)
+
+    try:
+        extract_list = list(response.json()['query']['pages'].values())
+        return [
+            d['*'] for d in extract_list[0]['extlinks']
+        ]
+    except KeyError as e:
+        logger.error(f'Failed to parse Wikimedia JSON: {response.json()}')
+        raise e
+
+
 def is_in_references(references, doi):
     """ Search for a doi in a list of references.
 
     Args:
-        references (iter): References to loop through
-        doi (str): DOI of work to search for
+        references (iter): References to loop through.
+        doi (str): DOI of work to search for.
 
     Returns:
-        bool: True if doi is in any of the references, else False
+        bool: True if doi is in any of the references, else False.
     """
     url_doi = doi.replace('/', '%2F')
 
     for reference in references:
         if url_doi in reference or doi in reference:
             return True
+
     return False
 
 
@@ -64,18 +153,27 @@ def check_wikipedia_event(event):
 
     """
     url = event.subject_id
-    page_language = get_language_from_wiki_url(url)
     doi = event.uri.raw
-    page_title = os.path.basename(url)
 
-    wikipedia.set_lang(page_language)
+    if is_wikipedia_url(url):
+        reference_list = get_wikipedia_references(url)
+    else:
+        reference_list = get_non_wikipedia_references(url)
 
-    try:
-        page_obj = wikipedia.WikipediaPage(title=unquote(page_title))
-    except PageError:
-        page_obj = wikipedia.page(title=unquote(page_title))
+    return is_in_references(reference_list, doi)
 
-    return is_in_references(page_obj.references, doi)
+
+def is_wikipedia_url(url):
+    """ Determine whether or not a URL is from a wikipedia page in the form
+    xy.wikipedia.org, where 'xy' the two-letter language code.
+
+    Args:
+        url (str): url of Wikimedia page.
+
+    Returns:
+        bool: True if the page is a Wikipedia page else False.
+    """
+    return bool(WIKI_PATTERN.search(url))
 
 
 def get_language_from_wiki_url(url, default_language='en'):
@@ -83,11 +181,11 @@ def get_language_from_wiki_url(url, default_language='en'):
     the language cannot be determined, English ('en') is set as default.
 
     Args:
-        url (str): URL of a Wikipedia page
-        default_language (str): code to return if language cannot be determined
+        url (str): URL of a Wikipedia page.
+        default_language (str): code to return if language cannot be determined.
 
     Returns:
-        str: 2-letter language code
+        str: two-letter language code.
     """
     match = WIKI_PATTERN.search(url)
     if match:
