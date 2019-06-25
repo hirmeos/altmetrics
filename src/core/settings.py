aa911fe946791d433e26bc4c0111d498e151bfab
rowan08 <g07h3212@gmail.com>
2019-06-25 17:51:11 +0100

    Bump version: 0.3.4 → 0.3.5

--- src/core/settings.py
+++ src/core/settings.py
@@ -108,7 +108,7 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    METRICS_VERSION = '0.3.4'
+    METRICS_VERSION = '0.3.5'
 
     # ## Twitter ##
     TWITTER_APP_KEY = getenv('TWITTER_APP_KEY')
