f44712451704c7d493c667f9b82b8c549a6e4737
rowan08 <g07h3212@gmail.com>
2019-09-03 13:42:09 +0100

    update requirements and remove xref-cited-by

--- src/core/settings.py
+++ src/core/settings.py
@@ -102,8 +102,6 @@ class Config:
             'generic',
             '__init__.py',
             '__pycache__',
-            'facebook',
-            'crossref_cited_by',
             'crossref_event_data',
         ]
     )
