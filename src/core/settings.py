d54dedcebc196a402fa938204311af724a839c11
rowan08 <g07h3212@gmail.com>
2021-04-07 19:26:39 +0100

    Bump version: 0.5.4 → 0.5.5

--- src/core/settings.py
+++ src/core/settings.py
@@ -55,7 +55,7 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    METRICS_VERSION = '0.5.4'
+    METRICS_VERSION = '0.5.5'
 
     # ## Twitter ##
     TWITTER_APP_KEY = environ.get('TWITTER_APP_KEY')
